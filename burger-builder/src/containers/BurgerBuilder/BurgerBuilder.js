cf60c0c4398f0b2611f8f0d2c6a8388997febdb6
Tanaya Karmakar <tkarmaka@Tanayas-MacBook-Pro.local>
2020-10-06 20:00:58 +0530

    code

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,31 +1,35 @@
 import React, {Component} from 'react';
-import Aux from '../../hoc/Aux';
+import {connect} from 'react-redux';
+import Aux from '../../hoc/Aux/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
-
-const INGREDIENT_PRICES = {
-    salad: 0.5,
-    cheese: 0.4,
-    meat: 1.3,
-    bacon: 0.7
-};
+import axios from '../../axios-orders';
+import Spinner from '../../components/UI/Spinner/Spinner';
+import * as actionTypes from '../../store/actions';
 
 class BurgerBuilder extends Component {
     state = {
-        ingredients: {
-            salad: 0,
-            bacon: 0,
-            cheese: 1,
-            meat: 0
-        },
-        totalPrice: 4,
-        purchasable: false,
-        purchasing: false
+        purchasing: false,
+        loading: false,
+        error: false
     };
 
-    addIngredientHandler = (type) => {
+    componentDidMount = () => {
+        // axios.get('/ingredients.json')
+        // .then(response => {
+        //     this.setState({
+        //         ingredients: response.data
+        //     });
+        // }).catch(error => {
+        //     this.setState({
+        //         error: true
+        //     });
+        // });
+    };
+
+    /*addIngredientHandler = (type) => {
         const oldCount = this.state.ingredients[type];
         const updatedCount = oldCount + 1;
         const updatedIngredients = {
@@ -57,7 +61,7 @@ class BurgerBuilder extends Component {
             ingredients: updatedIngredients
         });
         this.updatePurchaseState(updatedIngredients);
-    }
+    }*/
 
     updatePurchaseState = (ingredients) => { 
         const sum = Object.keys(ingredients)
@@ -67,9 +71,7 @@ class BurgerBuilder extends Component {
                 return sum + el;
             }, 0);
         
-        this.setState({
-            purchasable: sum > 0
-        });
+        return sum > 0;
     };
 
     purchaseHandler = () => {
@@ -85,36 +87,79 @@ class BurgerBuilder extends Component {
     };
 
     purchaseContinueHandler = () => {
-        alert('You continue!');
+        this.props.history.push('checkout');
+        // const queryParams = [];
+        // for(const i in this.state.ingredients) {
+        //     queryParams.push(encodeURIComponent(i) + 
+        //     '=' + encodeURIComponent(this.state.ingredients[i]));
+        // }
+        // queryParams.push('price=' + this.state.totalPrice);
+
+        // const queryString = queryParams.join('&');
+        // this.props.history.push({
+        //     pathname: '/checkout',
+        //     search: '?' + queryString
+        // });
     };
 
     render() {
-        const disabledInfo = {...this.state.ingredients};
+        const disabledInfo = {...this.props.ings};
         for(let key in disabledInfo) {
             disabledInfo[key] = disabledInfo[key] <= 0;
+        }
+
+        let burger = this.state.error ? <p>Ingredients can't be loaded !</p>: <Spinner />;
+        let orderSummary = null;
+        if(this.props.ings) {
+            burger = (
+                <Aux>
+                    <Burger ingredients = {this.props.ings}/>
+                    <BuildControls 
+                        ingredientAdded = {this.props.onIngredientAdded}
+                        ingredientRemoved = {this.props.onIngredientRemoved}
+                        disabled = {disabledInfo}
+                        price = {this.props.price}
+                        ordered = {this.purchaseHandler}
+                        purchasable = {this.updatePurchaseState(this.props.ings)}/>
+                </Aux>
+            );
+
+            orderSummary =   <OrderSummary 
+            ingredients = {this.props.ings}
+            purchaseCancelled = {this.purchaseCancelHandler}
+            purchaseContinued = {this.purchaseContinueHandler}
+            price = {this.props.price}/>;
+
+        }
+
+       
+        if(this.state.loading) {
+            orderSummary = <Spinner />;
         }
 
         return (
             <Aux>
                 <Modal show = {this.state.purchasing} modalClosed = {this.purchaseCancelHandler}>
-                    <OrderSummary 
-                        ingredients = {this.state.ingredients}
-                        purchaseCancelled = {this.purchaseCancelHandler}
-                        purchaseContinued = {this.purchaseContinueHandler}
-                        price = {this.state.totalPrice}
-                        />
+                  {orderSummary}
                 </Modal>
-                <Burger ingredients = {this.state.ingredients}/>
-                <BuildControls 
-                    ingredientAdded = {this.addIngredientHandler}
-                    ingredientRemoved = {this.removeIngredientHandler}
-                    disabled = {disabledInfo}
-                    price = {this.state.totalPrice}
-                    ordered = {this.purchaseHandler}
-                    purchasable = {this.state.purchasable}/>
+                {burger}
             </Aux>
         );
     }
 }
 
-export default BurgerBuilder;
+const mapStateToProps = state => {
+    return {
+        ings: state.ingredients,
+        price: state.totalPrice
+    }
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onIngredientAdded: (ingName) => dispatch({type: actionTypes.ADD_INGREDIENT, ingredientName: ingName}),
+        onIngredientRemoved: (ingName) => dispatch({type: actionTypes.REMOVE_INGREDIENT, ingredientName: ingName})
+    }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(BurgerBuilder);
--- burger-builder/src/index.js
+++ burger-builder/src/index.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './store/reducer';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const store = createStore(reducer);
+
+const app = (
+  <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>
+);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  app, document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
