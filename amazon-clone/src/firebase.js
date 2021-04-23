07fa96e6b1462ff1fb1241966b2f7a26b4a000d0
TanayaKarmakar <karmakar.tanaya@gmail.com>
2021-04-23 20:01:13 +0530

    code

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBhfQZENdBa8wbheZnRjjpJNNg7Ny6lWz0",
+    authDomain: "challenge-c56d1.firebaseapp.com",
+    projectId: "challenge-c56d1",
+    storageBucket: "challenge-c56d1.appspot.com",
+    messagingSenderId: "510026674686",
+    appId: "1:510026674686:web:37522dbac680a1ebab10af",
+    measurementId: "G-TT3R3J33FC"
+};
